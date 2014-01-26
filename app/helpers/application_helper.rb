9e375498ddf0d84f455f287fc4401dd84f413513
Afshin Mokhtari <afshinator@hotmail.com>
2014-01-26 07:53:13 +0000

    Add RSS feed to sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
+require 'rss/2.0'
+require 'open-uri'
+
+
 module ApplicationHelper
-  
   
   def title(value)
     unless value.nil?
@@ -16,6 +19,25 @@ module ApplicationHelper
       return "#{base}-#{controller.action_name}"
     end
   end  
-  
+
+    
+  # My Tumblr feed
+  def blog_feed
+      url = "http://afshinator.tumblr.com/rss"
+      
+      html = "<ul>"
+      
+      open(url) do |rss|
+        feed = RSS::Parser.parse(rss)
+        # puts "***********> Title: #{feed.channel.title}"
+        feed.items.each do |item|
+          html << "<li><a href='#{item.link}'>#{item.title}</a></li>"
+          # puts "++++++++++++> Item: #{item.title}"
+        end
+      end     
+      
+      html << "</ul>"
+      html.html_safe
+  end    
   
 end
