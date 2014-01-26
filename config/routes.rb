2cebaebbf8642b5286792adaf80a2e6e13b6c7bf
Afshin Mokhtari <afshinator@hotmail.com>
2014-01-26 06:19:54 +0000

    Add Apps as a dropdown in navi, not its own page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ MyPortfolioSite::Application.routes.draw do
   get "home", to: "pages#home", as: "home"
   get "about", to: "pages#about", as: "about"
   get "portfolio", to: "pages#portfolio", as: "portfolio"    
-  get "apps", to: "pages#apps", as: "apps"
   get "blog", to: "pages#blog", as: "blog"  
   get "inside", to: "pages#inside", as: "inside"
   
