54235249e045e164312d4fa0244d764fa4e2e838
Afshin Mokhtari <afshinator@hotmail.com>
2014-02-01 08:30:10 +0000

    Add recent posts to sidebar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,15 @@ class PagesController < ApplicationController
   ]
 
   def home
+      @all_articles = Article.order(created_at: :desc)     #how can i have this applied to all pages.. instead of using it explicitly on every page??    
   end
   
   def about
+      @all_articles = Article.order(created_at: :desc)     
   end
   
   def portfolio
+      @all_articles = Article.order(created_at: :desc)     
   end  
   
   def inside
