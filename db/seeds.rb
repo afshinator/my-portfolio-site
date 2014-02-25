b2b96bd57263600574a91e13bfae6d880ecfc183
Afshin Mokhtari <afshinator@hotmail.com>
2014-02-25 05:16:07 +0000

    update user system

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,19 @@
-# Generated with RailsBricks
 # Initial seed file to use with Devise User Model
 
 # Temporary admin account
+#u = User.new(
+#  username: "admin",
+#  email: "admin@example.com",
+#  password: "1234",
+#  password_confirmation: "1234",
+#  admin: true
+#)
+
 u = User.new(
-  username: "admin",
-  email: "admin@example.com",
-  password: "1234",
-  password_confirmation: "1234",
+  username: "afshin",
+  email: "afshinator@hotmail.com",
+  password: "afshin!Afshin",
+  password_confirmation: "afshin!Afshin",
   admin: true
 )
 u.skip_confirmation!
